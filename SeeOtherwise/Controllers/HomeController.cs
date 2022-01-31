e0f528ce4c204f1151121f72549c40d5782897b6
Mcoos11 <94370647+Mcoos11@users.noreply.github.com>
2022-01-31 17:52:02 +0100

    Add files via upload

--- SeeOtherwise/Controllers/HomeController.cs
+++ SeeOtherwise/Controllers/HomeController.cs
@@ -57,36 +57,189 @@ namespace SeeOtherwise.Controllers
             ViewBag.Photo = filter1.Photo;
             Image image1 = Image.FromFile(filter1.Photo);
 
-            //create a blank bitmap the same size as original
             Bitmap newBitmap = new Bitmap(image1);
+            Bitmap tmpBitmap = new Bitmap(image1);
+            Bitmap tmpBitmap2 = new Bitmap(image1);
+            Color IMGcolor;
+            int Rpx, Gpx, Bpx, ERpx, EGpx, EBpx; double Apx;
+            int rsume, gsume, bsume, Size = 3, margin = ((Size - 1) / 2);
+            int[] Filter = { -1, -1, -1, -1, 8, -1, -1, -1, -1 };
+            int nom = 1;
+            for (int i = 0; i < Size * Size; i++) nom += Filter[i];
 
-            //get a graphics object from the new image
-            Graphics g = Graphics.FromImage(newBitmap);
+            //gray
+            for (int i = 0; i < newBitmap.Width; i++)
+            {
+                for (int j = 0; j < newBitmap.Height; j++)
+                {
+                    IMGcolor = newBitmap.GetPixel(i, j);
+                    Rpx = (int)IMGcolor.R;
+                    Gpx = (int)IMGcolor.G;
+                    Bpx = (int)IMGcolor.B;
 
-            //create the grayscale ColorMatrix
-            ColorMatrix colorMatrix = new ColorMatrix(
-               new float[][]
-               {
-                new float[] {.3f, .3f, .3f, 0, 0},
-                new float[] {.59f, .59f, .59f, 0, 0},
-                new float[] {.11f, .11f, .11f, 0, 0},
-                new float[] {0, 0, 0, 1, 0},
-                new float[] {0, 0, 0, 0, 1}
-               });
+                    Bpx = (int)(Bpx * 0.114 + Gpx * 0.587 + Rpx * 0.299);
+                    Gpx = (int)(Bpx * 0.114 + Gpx * 0.587 + Rpx * 0.299);
+                    Rpx = (int)(Bpx * 0.114 + Gpx * 0.587 + Rpx * 0.299);
 
-            //create some image attributes
-            ImageAttributes attributes = new ImageAttributes();
+                    IMGcolor = Color.FromArgb((byte)Rpx, (byte)Gpx, (byte)Bpx);
 
-            //set the color matrix attribute
-            attributes.SetColorMatrix(colorMatrix);
+                    newBitmap.SetPixel(i, j, IMGcolor);
+                }
+            }
 
-            //draw the original image on the new image
-            //using the grayscale color matrix
-            g.DrawImage(newBitmap, new Rectangle(0, 0, newBitmap.Width, newBitmap.Height),
-               0, 0, newBitmap.Width, newBitmap.Height, GraphicsUnit.Pixel, attributes);
+            for (int i = margin; i < newBitmap.Width - margin; i++)
+            {
+                for (int j = margin; j < newBitmap.Height - margin; j++)
+                {
+                    rsume = 0;
+                    gsume = 0;
+                    bsume = 0;
+
+                    for (int k = 0; k < Size; k++)
+                        for (int l = 0; l < Size; l++)
+                        {
+                            IMGcolor = newBitmap.GetPixel(i + k - margin, j + l - margin);
+                            Rpx = (int)IMGcolor.R;
+                            Gpx = (int)IMGcolor.G;
+                            Bpx = (int)IMGcolor.B;
+
+                            rsume += Filter[k * Size + l] * Rpx;
+                            gsume += Filter[k * Size + l] * Gpx;
+                            bsume += Filter[k * Size + l] * Bpx;
+                        }
+
+                    rsume /= nom;
+                    gsume /= nom;
+                    bsume /= nom;
+
+                    if (rsume > 255) { rsume = 255; }
+                    else if (rsume < 0) { rsume = 0; }
+                    if (gsume > 255) { gsume = 255; }
+                    else if (gsume < 0) { gsume = 0; }
+                    if (bsume > 255) { bsume = 255; }
+                    else if (bsume < 0) { bsume = 0; }
+                    IMGcolor = Color.FromArgb((byte)rsume, (byte)gsume, (byte)bsume);
+                    tmpBitmap.SetPixel(i, j, IMGcolor);
+
+                }
+
+            }
+            for (int i = 0; i < newBitmap.Width; i++)
+            {
+                for (int j = 0; j < newBitmap.Height; j++)
+                {
+                    IMGcolor = tmpBitmap.GetPixel(i, j);
+                    Rpx = (int)IMGcolor.R;
+                    Gpx = (int)IMGcolor.G;
+                    Bpx = (int)IMGcolor.B;
+
+                    if (Rpx < 190)
+                    {
+                        Rpx = 255;
+                        Gpx = 255;
+                        Bpx = 255;
+                    }
+                    else if (Rpx >= 190)
+                    {
+                        Rpx = 0;
+                        Gpx = 0;
+                        Bpx = 0;
+                    }
+                    if (Rpx == 0)
+                    {
+                        Rpx = newBitmap.GetPixel(i, j).R;
+                        Gpx = newBitmap.GetPixel(i, j).G;
+                        Bpx = newBitmap.GetPixel(i, j).B;
+                    }
+                    else
+                    {
+                        Rpx = newBitmap.GetPixel(i, j).R + 100;
+                        if (Rpx > 255) { Rpx = 255; }
+                        Gpx = newBitmap.GetPixel(i, j).G + 100;
+                        if (Gpx > 255) { Gpx = 255; }
+                        Bpx = newBitmap.GetPixel(i, j).B + 100;
+                        if (Bpx > 255) { Bpx = 255; }
+                    }
+
+                    Bpx = (int)(Bpx * 0.114 + Gpx * 0.587 + Rpx * 0.299);
+                    Gpx = (int)(Bpx * 0.114 + Gpx * 0.587 + Rpx * 0.299);
+                    Rpx = (int)(Bpx * 0.114 + Gpx * 0.587 + Rpx * 0.299);
+
+                    IMGcolor = Color.FromArgb((byte)Rpx, (byte)Gpx, (byte)Bpx);
+                    tmpBitmap.SetPixel(i, j, IMGcolor);
+                    IMGcolor = Color.FromArgb((byte)Rpx, (byte)Gpx, (byte)Bpx);
+                    tmpBitmap.SetPixel(i, j, IMGcolor);
+                }
+
+            }
+
+
+            //rewriting
+            for (int i = 0; i < newBitmap.Width; i++)
+            {
+                for (int j = 0; j < newBitmap.Height; j++)
+                {
+                    newBitmap.SetPixel(i, j, tmpBitmap.GetPixel(i, j));
+                }
+            }
+
+            int[] Filter2 = { 1, 2, 1, 0, 0, 0, -1, -2, -1 };
+            nom = 1;
+            for (int i = 0; i < Size * Size; i++) nom += Filter2[i];
+
+            for (int i = margin; i < newBitmap.Width - margin; i++)
+            {
+                for (int j = margin; j < newBitmap.Height - margin; j++)
+                {
+                    rsume = 0;
+                    gsume = 0;
+                    bsume = 0;
+
+                    for (int k = 0; k < Size; k++)
+                        for (int l = 0; l < Size; l++)
+                        {
+                            IMGcolor = newBitmap.GetPixel(i + k - margin, j + l - margin);
+                            Rpx = (int)IMGcolor.R;
+                            Gpx = (int)IMGcolor.G;
+                            Bpx = (int)IMGcolor.B;
+
+                            rsume += Filter2[k * Size + l] * Rpx;
+                            gsume += Filter2[k * Size + l] * Gpx;
+                            bsume += Filter2[k * Size + l] * Bpx;
+                        }
+
+                    rsume /= nom;
+                    gsume /= nom;
+                    bsume /= nom;
+
+                    if (rsume > 255) { rsume = 255; }
+                    else if (rsume < 0) { rsume = 0; }
+                    if (gsume > 255) { gsume = 255; }
+                    else if (gsume < 0) { gsume = 0; }
+                    if (bsume > 255) { bsume = 255; }
+                    else if (bsume < 0) { bsume = 0; }
+
+                    rsume = 255 - rsume;
+                    gsume = 255 - gsume;
+                    bsume = 255 - bsume;
+
+                    IMGcolor = Color.FromArgb((byte)rsume, (byte)gsume, (byte)bsume);
+                    tmpBitmap.SetPixel(i, j, IMGcolor);
+
+                }
+
+            }
+            //rewriting
+            for (int i = 0; i < newBitmap.Width; i++)
+            {
+                for (int j = 0; j < newBitmap.Height; j++)
+                {
+                    newBitmap.SetPixel(i, j, tmpBitmap.GetPixel(i, j));
+                }
+            }
 
             //dispose the Graphics object
-            g.Dispose();
+            //g.Dispose();
               static byte[] BitmapToBytes(Image img)
             {
                 using (MemoryStream stream = new MemoryStream())
@@ -121,34 +274,179 @@ namespace SeeOtherwise.Controllers
             ViewBag.Photo = filter2.Photo;
             Image image2 = Image.FromFile(filter2.Photo);
 
-            //create a blank bitmap the same size as original
             Bitmap newBitmap = new Bitmap(image2);
+            Bitmap tmpBitmap = new Bitmap(image2);
+            Color IMGcolor;
+            int Rpx, Gpx, Bpx, ERpx, EGpx, EBpx; double Apx;
+            int rsume, gsume, bsume, Size = 3, margin = ((Size - 1) / 2);
+            int[] Filter = { -1, -1, -1, -1, 8, -1, -1, -1, -1 };
+            int nom = 1;
+            for (int i = 0; i < Size * Size; i++) nom += Filter[i];
 
-            //get a graphics object from the new image
-            Graphics g = Graphics.FromImage(newBitmap);
+            //gray
+            for (int i = 0; i < newBitmap.Width; i++)
+            {
+                for (int j = 0; j < newBitmap.Height; j++)
+                {
+                    IMGcolor = newBitmap.GetPixel(i, j);
+                    Rpx = (int)IMGcolor.R;
+                    Gpx = (int)IMGcolor.G;
+                    Bpx = (int)IMGcolor.B;
 
-            //create the grayscale ColorMatrix
-            ColorMatrix colorMatrix = new ColorMatrix(
-               new float[][]
-               {
-                new float[] {-1, 0, 0, 0, 0},
-                new float[] {0, -1, 0, 0, 0},
-                new float[] {0, 0, -1, 0, 0},
-                new float[] {0, 0, 0, 1, 0},
-                new float[] {1, 1, 1, 0, 1}
-               });
+                    Bpx = (int)(Bpx * 0.114 + Gpx * 0.587 + Rpx * 0.299);
+                    Gpx = (int)(Bpx * 0.114 + Gpx * 0.587 + Rpx * 0.299);
+                    Rpx = (int)(Bpx * 0.114 + Gpx * 0.587 + Rpx * 0.299);
 
-            //create some image attributes
-            ImageAttributes attributes = new ImageAttributes();
+                    IMGcolor = Color.FromArgb((byte)Rpx, (byte)Gpx, (byte)Bpx);
 
-            //set the color matrix attribute
-            attributes.SetColorMatrix(colorMatrix);
+                    newBitmap.SetPixel(i, j, IMGcolor);
+                }
+            }
 
-            //draw the original image on the new image
-            //using the grayscale color matrix
-            g.DrawImage(newBitmap, new Rectangle(0, 0, newBitmap.Width, newBitmap.Height),
-               0, 0, newBitmap.Width, newBitmap.Height, GraphicsUnit.Pixel, attributes);
+            for (int i = margin; i < newBitmap.Width - margin; i++)
+            {
+                for (int j = margin; j < newBitmap.Height - margin; j++)
+                {
+                    rsume = 0;
+                    gsume = 0;
+                    bsume = 0;
 
+                    for (int k = 0; k < Size; k++)
+                        for (int l = 0; l < Size; l++)
+                        {
+                            IMGcolor = newBitmap.GetPixel(i + k - margin, j + l - margin);
+                            Rpx = (int)IMGcolor.R;
+                            Gpx = (int)IMGcolor.G;
+                            Bpx = (int)IMGcolor.B;
+
+                            rsume += Filter[k * Size + l] * Rpx;
+                            gsume += Filter[k * Size + l] * Gpx;
+                            bsume += Filter[k * Size + l] * Bpx;
+                        }
+
+                    rsume /= nom;
+                    gsume /= nom;
+                    bsume /= nom;
+
+                    if (rsume > 255) { rsume = 255; }
+                    else if (rsume < 0) { rsume = 0; }
+                    if (gsume > 255) { gsume = 255; }
+                    else if (gsume < 0) { gsume = 0; }
+                    if (bsume > 255) { bsume = 255; }
+                    else if (bsume < 0) { bsume = 0; }
+                    IMGcolor = Color.FromArgb((byte)rsume, (byte)gsume, (byte)bsume);
+                    tmpBitmap.SetPixel(i, j, IMGcolor);
+
+                }
+
+            }
+            for (int i = 0; i < newBitmap.Width; i++)
+            {
+                for (int j = 0; j < newBitmap.Height; j++)
+                {
+                    IMGcolor = tmpBitmap.GetPixel(i, j);
+                    Rpx = (int)IMGcolor.R;
+                    Gpx = (int)IMGcolor.G;
+                    Bpx = (int)IMGcolor.B;
+
+                    if (Rpx < 190)
+                    {
+                        Rpx = 255;
+                        Gpx = 255;
+                        Bpx = 255;
+                    }
+                    else if (Rpx >= 190)
+                    {
+                        Rpx = 20;
+                        Gpx = 20;
+                        Bpx = 20;
+                    }
+
+                    if (Rpx == 20)
+                    {
+                        Rpx = newBitmap.GetPixel(i, j).R;
+                        Gpx = newBitmap.GetPixel(i, j).G;
+                        Bpx = newBitmap.GetPixel(i, j).B;
+                    }
+                    else
+                    {
+                        Rpx = newBitmap.GetPixel(i, j).R + 100;
+                        if (Rpx > 255) { Rpx = 255; }
+                        Gpx = newBitmap.GetPixel(i, j).G + 100;
+                        if (Gpx > 255) { Gpx = 255; }
+                        Bpx = newBitmap.GetPixel(i, j).B + 100;
+                        if (Bpx > 255) { Bpx = 255; }
+                    }
+
+                    Bpx = (int)(Bpx * 0.114 + Gpx * 0.587 + Rpx * 0.299);
+                    Gpx = (int)(Bpx * 0.114 + Gpx * 0.587 + Rpx * 0.299);
+                    Rpx = (int)(Bpx * 0.114 + Gpx * 0.587 + Rpx * 0.299);
+
+                    IMGcolor = Color.FromArgb((byte)Rpx, (byte)Gpx, (byte)Bpx);
+                    tmpBitmap.SetPixel(i, j, IMGcolor);
+                }
+            }
+
+            //rewriting
+            for (int i = 0; i < newBitmap.Width; i++)
+            {
+                for (int j = 0; j < newBitmap.Height; j++)
+                {
+                    newBitmap.SetPixel(i, j, tmpBitmap.GetPixel(i, j));
+                }
+            }
+
+            int[] Filter2 = { 1, 1, 1, 0, 1, 0, -1, -1, -1 };
+            nom = 0;
+            for (int i = 0; i < Size * Size; i++) nom += Filter2[i];
+
+            for (int i = margin; i < newBitmap.Width - margin; i++)
+            {
+                for (int j = margin; j < newBitmap.Height - margin; j++)
+                {
+                    rsume = 0;
+                    gsume = 0;
+                    bsume = 0;
+
+                    for (int k = 0; k < Size; k++)
+                        for (int l = 0; l < Size; l++)
+                        {
+                            IMGcolor = newBitmap.GetPixel(i + k - margin, j + l - margin);
+                            Rpx = (int)IMGcolor.R;
+                            Gpx = (int)IMGcolor.G;
+                            Bpx = (int)IMGcolor.B;
+
+                            rsume += Filter2[k * Size + l] * Rpx;
+                            gsume += Filter2[k * Size + l] * Gpx;
+                            bsume += Filter2[k * Size + l] * Bpx;
+                        }
+
+                    rsume /= nom;
+                    gsume /= nom;
+                    bsume /= nom;
+
+                    if (rsume > 255) { rsume = 255; }
+                    else if (rsume < 0) { rsume = 0; }
+                    if (gsume > 255) { gsume = 255; }
+                    else if (gsume < 0) { gsume = 0; }
+                    if (bsume > 255) { bsume = 255; }
+                    else if (bsume < 0) { bsume = 0; }
+
+                    IMGcolor = Color.FromArgb((byte)rsume, (byte)gsume, (byte)bsume);
+                    tmpBitmap.SetPixel(i, j, IMGcolor);
+
+                }
+
+            }
+
+            //rewriting
+            for (int i = 0; i < newBitmap.Width; i++)
+            {
+                for (int j = 0; j < newBitmap.Height; j++)
+                {
+                    newBitmap.SetPixel(i, j, tmpBitmap.GetPixel(i, j));
+                }
+            }
             //dispose the Graphics object
             //g.Dispose();
             static byte[] BitmapToBytes(Image img)
@@ -194,11 +492,11 @@ namespace SeeOtherwise.Controllers
             ColorMatrix colorMatrix = new ColorMatrix(
                new float[][]
                {
-                new float[] {1.5f, 0 , 0, 0, 0}, // this is for red
-                new float[] {0, 1.4f, 0, 0, 0}, // this is for green
-                new float[] {0, 0, .8f, 0, 0}, // this is for blue
-                new float[] {0, 0, 0, .8f, 0}, // this is alpha
-                new float[] {0, 0, 0, 0, 1}
+                new float[] {-1, 0, 0, 0, 0},
+                new float[] {0, -1, 0, 0, 0},
+                new float[] {0, 0, -1, 0, 0},
+                new float[] {0, 0, 0, 1, 0},
+                new float[] {1, 1, 1, 0, 1}
                });
 
             //create some image attributes
@@ -211,6 +509,8 @@ namespace SeeOtherwise.Controllers
             //using the grayscale color matrix
             g.DrawImage(newBitmap, new Rectangle(0, 0, newBitmap.Width, newBitmap.Height),
                0, 0, newBitmap.Width, newBitmap.Height, GraphicsUnit.Pixel, attributes);
+
+
 
             //dispose the Graphics object
             //g.Dispose();
@@ -228,7 +528,6 @@ namespace SeeOtherwise.Controllers
             return View("filter3w");
         }
 
-      
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
